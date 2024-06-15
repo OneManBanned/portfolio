afd6f8f21dd4e46915545de86f7a6dbe8e94cdca
Brendan <brendanlawless@hotmail.com>
2024-06-15 21:21:55 +0100

    init commit

--- src/server/main.ts
+++ src/server/main.ts
@@ -9,65 +9,64 @@ const app = express();
 app.use(express.json());
 
 app.post("/contact-form", async (req, res) => {
-  const body = req.body;
-  const result = ContactSchema.safeParse(body);
+    const body = req.body;
+    const result = ContactSchema.safeParse(body);
 
-  if (result.success) {
-    let transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: process.env.MAIL_USERNAME,
-        clientId: process.env.OAUTH_CLIENTID,
-        clientSecret: process.env.OAUTH_CLIENT_SECRET,
-        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-      },
-    });
+    if (result.success) {
+        let transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                type: "OAuth2",
+                user: process.env.MAIL_USERNAME,
+                clientId: process.env.OAUTH_CLIENTID,
+                clientSecret: process.env.OAUTH_CLIENT_SECRET,
+                refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+            },
+        });
 
-    const { name, email, message } = result.data;
+        const { name, email, message } = result.data;
 
-    await new Promise((res, rej) => {
-      transporter.verify(function (error, success) {
-        if (error) {
-          console.log(error);
-          rej(error);
-        } else {
-          console.log("Server is ready to take our messages");
-          res(success);
-        }
-      });
-    });
+        await new Promise((res, rej) => {
+            transporter.verify(function(error, success) {
+                if (error) {
+                    console.log(error);
+                    rej(error);
+                } else {
+                    console.log("Server is ready to take our messages");
+                    res(success);
+                }
+            });
+        });
 
-    let mailOptions = {
-      from: email,
-      replyTo: email,
-      to: "lawlesswebdev@gmail.com",
-      subject: `${name} sent a message.`,
-      text: message,
-    };
+        let mailOptions = {
+            from: email,
+            replyTo: email,
+            to: "lawlesswebdev@gmail.com",
+            subject: `${name} sent a message.`,
+            text: message,
+        };
 
-    await new Promise((response, reject) => {
-      transporter.sendMail(mailOptions, function (err, data) {
-        if (err) {
-          console.error(err);
-          reject(err);
-        } else {
-          response(data);
-        }
-      });
-    });
+        await new Promise((response, reject) => {
+            transporter.sendMail(mailOptions, function(err, data) {
+                if (err) {
+                    console.error(err);
+                    reject(err);
+                } else {
+                    response(data);
+                }
+            });
+        });
 
-    return res.status(200).json({ success: true });
+        return res.status(200).json({ success: true });
+    }
 
-  }
+    const serverErrors = Object.fromEntries(
+        result.error?.issues?.map((issue) => [issue.path[0], issue.message]) || [],
+    );
 
-  const serverErrors = Object.fromEntries(
-    result.error?.issues?.map((issue) => [issue.path[0], issue.message]) || [],
-  );
-
-  return res.json({ errors: serverErrors });
+    return res.json({ errors: serverErrors });
 });
 
 ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+    console.log("Server is listening on port 3000..."),
 );
